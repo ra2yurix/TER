d45cdaebb6329ab7716ab167085e9b3584053e6f
ra2yurix <ra2yurix@msn.com>
2021-12-16 16:38:26 +0100

    show result

--- apps/search/views.py
+++ apps/search/views.py
@@ -9,7 +9,7 @@ search_engine = SearchEngine()
 
 
 def index(request):
-    print("req", request)
+    print("req", request.method)
     if request.method == "POST":
         print("2222")
         start_time = time.time()
@@ -40,37 +40,37 @@ def index(request):
                            'elapsed': elapsed_time, 'number': 1, 'year': year, 'rating': rating, 'results': result})
 
     return render(request, "search.html", {"search_text": ""})
-
-def filter(request):
-    res = request.GET.getlist("result")
-    print(res)
-    rating = request.GET.get("rating")
-    year = request.GET.get("year")
-    query = request.GET.get("search_text")
-    # genre_list = request.GET.getlist('multi_genre')
-    # date_q = QRY.DateRange("release_date", datetime.strptime(year.split(",")[0], "%Y"),datetime.strptime(year.split(",")[1], "%Y"))
-    # rating_q = QRY.NumericRange("vote_average",int(rating.split(",")[0]), int(rating.split(",")[1]))
-    # filter_q = QRY.And([date_q, rating_q])
-    # if len(genre_list) > 0:
-    #     genres_q=QRY.Or([QRY.Term(u"genres",unicode(x.lower())) for x in genre_list])
-    #     filter_q = QRY.And([filter_q, genres_q])
-    # ix = i.open_dir(INDEX_FILE)
-    # searcher = ix.searcher(weighting=scoring.TF_IDF())
-    # hitsList=[]
-    # for x in res:
-    #     res_q = QRY.Term(u"id", unicode(x))
-    #     filter = QRY.And([filter_q, res_q])
-    #     temp_hit = searcher.search(filter, filter=None, limit=None)
-    #     if temp_hit:
-    #         for y in temp_hit:
-    #             hitsList.append(y)
-
-
-
-
-    #
-    # print(filter_q)
-
-    return render(request, 'search.html',
-                  { 'search_text': query,
-                    'results':res})
+#
+# def filter(request):
+#     res = request.GET.getlist("result")
+#     print(res)
+#     rating = request.GET.get("rating")
+#     year = request.GET.get("year")
+#     query = request.GET.get("search_text")
+#     # genre_list = request.GET.getlist('multi_genre')
+#     # date_q = QRY.DateRange("release_date", datetime.strptime(year.split(",")[0], "%Y"),datetime.strptime(year.split(",")[1], "%Y"))
+#     # rating_q = QRY.NumericRange("vote_average",int(rating.split(",")[0]), int(rating.split(",")[1]))
+#     # filter_q = QRY.And([date_q, rating_q])
+#     # if len(genre_list) > 0:
+#     #     genres_q=QRY.Or([QRY.Term(u"genres",unicode(x.lower())) for x in genre_list])
+#     #     filter_q = QRY.And([filter_q, genres_q])
+#     # ix = i.open_dir(INDEX_FILE)
+#     # searcher = ix.searcher(weighting=scoring.TF_IDF())
+#     # hitsList=[]
+#     # for x in res:
+#     #     res_q = QRY.Term(u"id", unicode(x))
+#     #     filter = QRY.And([filter_q, res_q])
+#     #     temp_hit = searcher.search(filter, filter=None, limit=None)
+#     #     if temp_hit:
+#     #         for y in temp_hit:
+#     #             hitsList.append(y)
+#
+#
+#
+#
+#     #
+#     # print(filter_q)
+#
+#     return render(request, 'search.html',
+#                   { 'search_text': query,
+#                     'results':res})
