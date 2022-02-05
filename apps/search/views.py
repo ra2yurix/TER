8fad8809c53a19fdd5c3cde7c924a9850d030427
ChangjunDAI <dai.changjun@outlook.com>
2022-02-05 21:22:05 +0100

    2022020501

--- apps/search/views.py
+++ apps/search/views.py
@@ -18,15 +18,5 @@ def search(request):
     search_text = request.GET.get("search_text")
     results = search_engine.text_query(search_text)
     fileImage = request.FILES.get('uploadPicture')
-    if not results:
-        return render(request, "noresult.html")
-    else:
-        # new_img = Image.open(fileImage)
-        # new_img = new_img.crop()
-        # extract_label = res_comparer(new_img)
-        print("-----")
-        print(results[0])
-        print("-----000")
-        print(results[0]["genres"])
-        # return render(request, "search.html")
+    if results:
         return render(request, "search.html", {'hits': results})
